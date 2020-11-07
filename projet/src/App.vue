75d8a08578855e1e6c81b4f16d41df0990b685c0
eabdelrazak <43001225+eabdelrazak@users.noreply.github.com>
2020-11-07 20:54:37 +0100

    Ajout de la base du projet

---
+++ projet/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ListeDesRestaurant msg="Coucou"/>
+  </div>
+</template>
+
+<script>
+import ListeDesRestaurant from './components/ListeDesRestaurant.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ListeDesRestaurant
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
